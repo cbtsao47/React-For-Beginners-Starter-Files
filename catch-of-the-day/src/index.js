7573e1af66f52b8f6c056121ee30e072fd470b0f
wisdomtooth <cbtsao47@gmail.com>
2019-03-18 01:15:53 -0500

    initial commit

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from "react";
+import ReactDOM from "react-dom";
+import StorePicker from "./StorePicker";
+import App from "./App";
+import "./css/style.css";
+
+ReactDOM.render(<App />, document.getElementById("main"));
